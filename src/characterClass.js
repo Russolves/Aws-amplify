43c97fe16ab310aa9b1c716ee61cf9650a55764b
Russell Ho <russellhohoho@gmail.com>
2024-09-23 19:07:45 -0400

    Experimentation frontend

---
+++ src/characterClass.js
@@ -0,0 +1,144 @@
+export default class characterClass {
+  constructor(x, y, dx, dy, character, fontSize, color, number, pathType = 'linear') {
+    this.character = character; // Character to display
+    this.fontSize = fontSize; // Size of the character
+    this.color = color; // Color of the character
+    this.number = number; // Number of soldiers
+    this.pathType = pathType; // 'linear', 'sin', 'cos'
+    this.angle = 0; // Used for sine and cosine
+
+    // Create an array to store positions and velocities for each summoned character
+    this.soldiers = Array.from({ length: this.number }, () =>
+      new this.Soldier(x, y, dx, dy, character, fontSize, color, pathType)
+    );
+  }
+
+  // Nested Soldier class
+  Soldier = class {
+    constructor(x, y, dx, dy, character, fontSize, color, pathType = 'linear') {
+      (character === 'D') ? this.x = x : this.x = x + (Math.random() - 0.5) * 250; // Random initial position around the main character
+      (character === 'D') ? this.y = y : this.y = y + (Math.random() - 0.5) * 250;
+      this.dx = dx;
+      this.dy = dy;
+      this.character = character;
+      this.fontSize = fontSize;
+      this.color = color;
+      this.pathType = pathType;
+      this.angle = 0;
+    }
+
+    draw(ctx) {
+      ctx.font = `${this.fontSize}px Arial`;
+      ctx.fillStyle = this.color;
+      ctx.fillText(this.character, this.x, this.y);
+    }
+
+    update(canvas) {
+      switch (this.pathType) {
+        case 'sin':
+          this.x += this.dx;
+          this.y = canvas.height / 2 + Math.sin(this.angle) * 100; // Amplitude of 100
+          this.angle += 0.010; // Controls the speed of the sine wave
+          break;
+        case 'cos':
+          this.x += this.dx;
+          this.y = canvas.height / 2 + Math.cos(this.angle) * 100; // Amplitude of 100
+          this.angle += 0.025; // Controls the speed of the cosine wave
+          break;
+        case 'circular':
+          this.x = canvas.width / 2 + Math.cos(this.angle) * 100; // Circular path with radius 100
+          this.y = canvas.height / 2 + Math.sin(this.angle) * 100;
+          this.angle += 0.03;
+          break;
+        default:
+          this.x += this.dx;
+          this.y += this.dy;
+          break;
+      }
+      // Bounce off the walls
+      if (this.x + this.fontSize > canvas.width || this.x < 0) {
+        this.dx = -this.dx;
+      }
+      if (this.y + this.fontSize > canvas.height || this.y - this.fontSize < 0) {
+        this.dy = -this.dy;
+      }
+    }
+  };
+
+  // Get all soldier's positions
+  get() {
+    let all_x = this.soldiers.map(soldier => soldier.x);
+    let all_y = this.soldiers.map(soldier => soldier.y);
+    return [all_x, all_y];
+  }
+
+  // Draw each soldier on the canvas
+  draw(ctx) {
+    this.soldiers.forEach(soldier => soldier.draw(ctx));
+  }
+
+  // Hierarchy of power
+  hierarchy(race) {
+    if (this.character === 'N' && race === 'aeldari') return true;
+    if (this.character === 'A' && race === 'tyranid') return true;
+    if (this.character === 'T' && race === 'spaceMarine') return true;
+    if (this.character === 'S' && race === 'necron') return true;
+    return false;
+  }
+
+  // Update each soldier's position and handle boundaries
+  update(ctx, canvas, other_pos, death_mode) {
+    this.soldiers.forEach(soldier => soldier.update(canvas));
+
+    // Bounce off other soldiers
+    for (let i = 0; i < this.soldiers.length; i++) {
+      for (let j = 0; j < this.soldiers.length; j++) {
+        if (i !== j) {
+          let soldier1 = this.soldiers[i];
+          let soldier2 = this.soldiers[j];
+          let distance = Math.sqrt(
+            Math.pow(soldier1.x - soldier2.x, 2) + Math.pow(soldier1.y - soldier2.y, 2)
+          );
+          if (distance < this.fontSize) {
+            soldier1.dy = -soldier1.dy;
+            soldier2.dx = -soldier2.dx;
+            soldier2.dy = -soldier2.dy;
+          }
+        }
+      }
+    }
+
+    // Bounce off enemy soldiers
+    for (let race in other_pos) {
+      if (!other_pos[race]) continue;
+      const race_x = other_pos[race][0];
+      const race_y = other_pos[race][1];
+      let i = 0;
+      while (i < this.soldiers.length) {
+        let current_soldier = this.soldiers[i];
+        for (let k = 0; k < race_x.length; k++) {
+          let distance = Math.sqrt(
+            Math.pow(current_soldier.x - race_x[k], 2) + Math.pow(current_soldier.y - race_y[k], 2)
+          );
+          if (distance < this.fontSize) {
+            if (this.hierarchy(race)) {
+              if (!death_mode) {
+                // Clear the area where the character was drawn
+                ctx.clearRect(current_soldier.x - this.fontSize / 2,
+                  current_soldier.y - this.fontSize,
+                  this.fontSize, this.fontSize);
+                this.soldiers.splice(i, 1); // Remove the current soldier from the soldiers' array
+              } else {
+              this.soldiers.splice(i, 1, new this.Soldier(current_soldier.x, current_soldier.y, 0, 0, 'D', 30, 'green')) // Or replace with dead
+              }
+            } else {
+              current_soldier.dx = -current_soldier.dx;
+              // current_soldier.dy = -current_soldier.dy;
+            }
+          }
+        }
+        i += 1;
+      }
+    }
+  }
+}
---
+++ src/pages/Main.jsx
@@ -0,0 +1,199 @@
+import React, { useState, useEffect, useCallback, useRef } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import characterClass from '../characterClass';
+
+function Main() {
+    const [dead, setDead] = useState(false);
+    const [isAnimated, setAnimated] = useState(false);
+    const [currentSelected, setCurrentSelected] = useState('');
+    const [selectedOptions, setSelectedOptions] = useState([]);
+    const [selectedPattern, setSelectedPattern] = useState('');
+    const [races, setRaces] = useState({
+        space_marines: { use: false, number: 1, speed: 5, pattern: null },
+        necrons: { use: false, number: 1, speed: 5, pattern: null },
+        aeldari: { use: false, number: 1, speed: 5, pattern: null },
+        tyranids: { use: false, number: 1, speed: 5, pattern: null }
+    });
+    const [whoWon, setwhoWon] = useState('');
+    // const [image, setImage] = useState('http://localhost:8000/images/spaceparanoids.png');
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        // Create instances of BouncingCharacter
+        let spaceMarine = null;
+        if (races.space_marines.use) spaceMarine = new characterClass(100, 100, races.space_marines.speed, 4, 'S', 30, 'blue', races.space_marines.number, races.space_marines.pattern);
+        let necron = null;
+        if (races.necrons.use) necron = new characterClass(1000, 300, races.necrons.speed, 2, 'N', 30, 'black', races.necrons.number, races.necrons.pattern);
+        let aeldari = null;
+        if (races.aeldari.use) aeldari = new characterClass(100, 300, races.aeldari.speed, 3, 'A', 30, 'red', races.aeldari.number, races.aeldari.pattern);
+        let tyranid = null;
+        if (races.tyranids.use) tyranid = new characterClass(1000, 100, races.tyranids.speed, 3, 'T', 30, 'purple', races.tyranids.number, races.tyranids.pattern);
+        let animationId; // Store the animation frame ID
+        let factions = 0;
+        for (let key in races) {
+            if (!races[key].use) {
+                factions += 1;
+            }
+        }
+        if (factions === 4) { setAnimated(false); return }
+        function draw() {
+            // Clear the canvas
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            // get all characters' positions and draw & update animation
+            let spaceMarine_pos;
+            (spaceMarine !== null) ? spaceMarine_pos = spaceMarine.get() : spaceMarine_pos = null;
+            let necron_pos;
+            (necron !== null) ? necron_pos = necron.get() : necron_pos = null;
+            let aeldari_pos;
+            (aeldari !== null) ? aeldari_pos = aeldari.get() : aeldari_pos = null;
+            let tyranid_pos;
+            (tyranid !== null) ? tyranid_pos = tyranid.get() : tyranid_pos = null;
+            // Printing out who won
+            let winning_race = {};
+            if (spaceMarine !== null) {
+                spaceMarine.draw(ctx);
+                spaceMarine.update(ctx, canvas, { necron: necron_pos, aeldari: aeldari_pos, tyranid: tyranid_pos }, dead);
+                winning_race["Space Marines"] = spaceMarine_pos[0]; // for printing out who won
+            }
+            if (necron !== null) {
+                necron.draw(ctx);
+                necron.update(ctx, canvas, { spaceMarine: spaceMarine_pos, aeldari: aeldari_pos, tyranid: tyranid_pos }, dead);
+                winning_race["Necrons"] = necron_pos[0];
+            }
+            if (aeldari !== null) {
+                aeldari.draw(ctx);
+                aeldari.update(ctx, canvas, { spaceMarine: spaceMarine_pos, necron: necron_pos, tyranid: tyranid_pos }, dead);
+                winning_race["Aeldari"] = aeldari_pos[0];
+            }
+            if (tyranid !== null) {
+                // draw and update animation
+                tyranid.draw(ctx);
+                tyranid.update(ctx, canvas, { spaceMarine: spaceMarine_pos, aeldari: aeldari_pos, necron: necron_pos }, dead);
+                winning_race["Tyranids"] = tyranid_pos[0];
+            }
+
+            // Continue animation only if isAnimated is true
+            if (isAnimated) {
+                animationId = requestAnimationFrame(draw);
+            }
+            winning_race = Object.fromEntries(Object.entries(winning_race).filter(([_, pos]) => pos.length > 0)); // Remove empty winning_race
+            if (Object.keys(winning_race).length === 2 && factions !== 2) {
+                let prev = null;
+                for (let race in winning_race) {
+                    if (prev !== null) {
+                        if (winning_race[race].length > winning_race[prev].length) {
+                            setwhoWon(`The ${race} win!!`);
+                        } else if (winning_race[race].length < winning_race[prev].length) {
+                            setwhoWon(`The ${prev} win!!`);
+                        } else {
+                            setwhoWon(`It's a draw between the ${race} and ${prev}`)
+                        }
+                    }
+                    prev = race;
+                }
+            } else if (factions === 2) {
+                const deathmatch = Object.keys(winning_race);
+                const race1 = deathmatch[0];
+                const race2 = deathmatch[1];
+                if (!winning_race[race1]) {
+                    setwhoWon(`The ${race2} win the deathmatch!`);
+                } else if (!winning_race[race2]) {
+                    setwhoWon(`The ${race1} win the deathmatch!`);
+                }
+            }
+        }
+        if (isAnimated) {
+            // Start the animation
+            animationId = requestAnimationFrame(draw);
+        }
+        // Cleanup to stop animation on component unmount
+        return () => cancelAnimationFrame(animationId);
+    }, [isAnimated]); // Only restart the effect when isAnimated changes
+
+    // Dead mode
+    const toggleDead = () => {
+        const death = !dead;
+        setDead(death);
+    }
+
+    // For multi dropdown select
+    const handleSelectChange = (event) => {
+        const options = Array.from(event.target.selectedOptions, option => option.value);
+        setSelectedOptions(options);
+        for (let race of options) {
+            setCurrentSelected(race);
+            races[race].use = !races[race].use;
+        }
+    };
+    // for speed
+    const handleSpeedChange = (event, current) => {
+        const speed = event.target.value;
+        if (!isNaN(speed) && !isNaN(parseFloat(speed))) {
+            races[current].speed = parseInt(event.target.value);
+        } else {
+            event.target.value = null;
+        }
+    }
+    // for number of units in army
+    const handleNumberChange = (event, current) => {
+        const number = event.target.value;
+        if (!isNaN(number) && !isNaN(parseFloat(number))) {
+            races[current].number = parseInt(event.target.value);
+        } else {
+            event.target.value = null;
+        }
+    }
+    // for pattern of army faction
+    const handlePatternChange = (event) => {
+        const pattern = event.target.value;
+        races[currentSelected].pattern = pattern;
+    }
+    const animationStop = () => {
+        setAnimated(!isAnimated);
+        setwhoWon('');
+    };
+
+    return (
+        <div>
+            {(isAnimated) ? (
+                <h1>Fight! {name}</h1>
+            ) : (<h1>Russell's Warhammer 40K Simulation</h1>)}
+            <button onClick={toggleDead}>Dead mode</button>
+            <button id="toggleButton" onClick={animationStop}>Stop/Start</button>
+            <select multiple value={selectedOptions} onChange={handleSelectChange} style={{ minHeight: '5rem', minWidth: '10rem', marginLeft: '1rem', marginRight: '1rem' }}>
+                {(!races.space_marines.use) ? (<option value="space_marines">Space Marines</option>) : (<option value="space_marines">Space Marines    ✓</option>)}
+                {(!races.necrons.use) ? (<option value="necrons">Necrons</option>) : (<option value="necrons">Necrons    ✓</option>)}
+                {(!races.aeldari.use) ? (<option value="aeldari">Aeldari</option>) : (<option value="aeldari">Aeldari    ✓</option>)}
+                {(!races.tyranids.use) ? (<option value="tyranids">Tyranids</option>) : (<option value="tyranids">Tyranids    ✓</option>)}
+            </select>
+            <input style={{ maxWidth: '10rem' }} onChange={(e) => handleSpeedChange(e, currentSelected)} placeholder={`Enter Speed for ${currentSelected}`} />
+            <input style={{ maxWidth: '10rem' }} onChange={(e) => handleNumberChange(e, currentSelected)} placeholder={`Enter # units for ${currentSelected}`} />
+            <select value={selectedPattern} onChange={handlePatternChange}>
+                <option value="default">Default</option>
+                <option value="sin">Sin</option>
+                <option value="cos">Cos</option>
+                <option value="circular">Circular</option>
+            </select>
+            {/* <button onClick={changeImage}>Switch Image</button> */}
+            <div style={{ flex: 'start', justifyContent: 'center', alignItems: 'center' }}>
+                <canvas ref={canvasRef} style={{ border: '1px solid black', maxWidth: '50rem' }} />
+            </div>
+            {(whoWon !== '') && (
+                <h1>{whoWon}</h1>
+            )}
+            {/* <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+                <img src={image} alt="Dynamic Image Example" style={{ maxWidth: '60rem', maxHeight: '60rem' }} />
+            </div> */}
+        </div>
+    );
+}
+
+export default Main;
